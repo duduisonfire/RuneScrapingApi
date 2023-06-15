ac77ef8e35d25680784adddc7ce011982789ce11
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-06-14 21:43:39 -0300

    Added an startup cache verify and scheduled sync.

--- RunesWebScraping/Program.cs
+++ RunesWebScraping/Program.cs
@@ -2,6 +2,7 @@ using RunesWebScraping.services;
 using dotenv.net;
 using RunesWebScraping;
 using RunesWebScraping.domain;
+using CronNET;
 
 DotEnv.Load();
 var builder = WebApplication.CreateBuilder(args);
@@ -14,8 +15,32 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<UggService>();
 builder.Services.AddSingleton<RuneCacheSync>();
 builder.Services.AddSingleton<LolApi>();
+builder.Services.AddSingleton<CacheManager>();
 
-var app = builder.Build(); 
+
+
+var app = builder.Build();
+var cacheManager = app.Services.GetRequiredService<CacheManager>();
+
+var lifetime = app.Lifetime;
+lifetime.ApplicationStarted.Register(async () => {
+    var hasCache = await cacheManager.VerifyCache();
+
+    if (!hasCache)
+    {
+        await cacheManager.SyncCache();
+        Console.WriteLine("Syncing the cache");
+    } else {
+        Console.WriteLine("Have cache");
+    }
+
+    ThreadStart thread = new(async () => {
+        await cacheManager.SyncCache();
+    });
+
+    var cron = new CronDaemon();
+    cron.AddJob("* 22 * * *", thread);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
