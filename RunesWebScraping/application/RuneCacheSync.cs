0aabc25f606f8be911ab8242fdd2af96e8b93699
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-06-28 13:10:26 -0300

    Fixes bug that crashes the app if it fails in webscrap.

--- RunesWebScraping/application/RuneCacheSync.cs
+++ RunesWebScraping/application/RuneCacheSync.cs
@@ -18,29 +18,30 @@ public class RuneCacheSync
 
     private async Task UpdateChampionCache(string champion, string lane)
     {
-        try
+        while (true)
         {
-            var webScrap = new RuneWebScrap(champion, lane);
-            await webScrap.GetRunes();
-            var runesId = new RunePage(webScrap.runeList);
-            var runeResponse = new RuneResponse(webScrap, runesId);
-
-            var ChampionCacheExists = await _uggDbService.ChampionCacheExists(champion, lane);
-
-            if (ChampionCacheExists != null)
+                try
             {
-                await _uggDbService.UpdateChampionCache(runeResponse);
-                Console.WriteLine($"The runes of {champion} in {lane} are updated.");
-                return;
-            }
+                var webScrap = new RuneWebScrap(champion, lane);
+                await webScrap.GetRunes();
+                var runesId = new RunePage(webScrap.runeList);
+                var runeResponse = new RuneResponse(webScrap, runesId);
 
-            await _uggDbService.CreateChampionCache(runeResponse);
-            Console.WriteLine($"The runes of {champion} in {lane} are created.");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-            throw;
+                var ChampionCacheExists = await _uggDbService.ChampionCacheExists(champion, lane);
+
+                if (ChampionCacheExists != null)
+                {
+                    await _uggDbService.UpdateChampionCache(runeResponse);
+                    Console.WriteLine($"The runes of {champion} in {lane} are updated.");
+                    return;
+                }
+                await _uggDbService.CreateChampionCache(runeResponse);
+                Console.WriteLine($"The runes of {champion} in {lane} are created.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 
@@ -56,7 +57,7 @@ public class RuneCacheSync
                 await UpdateChampionCache(championList[i], "top");
                 await UpdateChampionCache(championList[i], "jungle");
                 await UpdateChampionCache(championList[i], "adc");
-                await UpdateChampionCache(championList[i], "support");
+                await UpdateChampionCache(championList[i], "supp");
 
                 Console.WriteLine($"Updated {championList[i]} runes cache.");
             }
